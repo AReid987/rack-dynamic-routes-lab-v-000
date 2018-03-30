cca53bb966b44b1bdb1c2804d3aeddff7b24a2d3
Antonio Reid <read.musik@gmail.com>
2018-03-30 02:56:46 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,9 @@ class Application
     #binding.pry
     if req.path.match(/item/)
       item_name = req.path.split("/items/").last
+      #if @@items.find{|i| i.name == item_name}
+        
+      #end
       binding.pry
     else
       resp.write "Route not found"
