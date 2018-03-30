fce0a034b7986ea2851e23d08c5bc5f84af456fe
Antonio Reid <read.musik@gmail.com>
2018-03-30 02:50:20 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,9 +3,9 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    binding.pry 
+    binding.pry
     if req.path.match(/item/)
-      item = req.path.split("items").last
+      item = req.path.split("/items/").last
     else
       resp.write "Route not found"
       resp.status = 404
