3c971ad9dd9f1c4a63fca33a546b595403fcb196
Antonio Reid <read.musik@gmail.com>
2018-03-30 03:00:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     if req.path.match(/item/)
       item_name = req.path.split("/items/").last
       if !@@items.find{|i| i.name == item_name}.nil?
-
+        item = @@items.find{|i| i.name == item_name}.price 
       end
       binding.pry
     else
