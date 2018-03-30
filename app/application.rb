2819e163130ee30b69f0e7679f28e3e73bf0ab09
Antonio Reid <read.musik@gmail.com>
2018-03-30 02:45:59 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/item/)
-      item = req.path.split("items").last 
+      item = req.path.split("items").last
       binding.pry 
     end
   end
