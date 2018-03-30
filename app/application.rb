f172e5bcec7ad78cbc0a5387a68f6eafaca6fa18
Antonio Reid <read.musik@gmail.com>
2018-03-30 03:00:49 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,6 +4,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+    
     if req.path.match(/item/)
       item_name = req.path.split("/items/").last
       if !@@items.find{|i| i.name == item_name}.nil?
